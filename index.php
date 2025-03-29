12d6aa716b283490d104b2efa6b64eb4db39fe5e
kennyrey14 <magsayokennyrey30@gmail.com>
2025-03-29 14:00:29 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<table>
+  <tr>
+    <th>Company</th>
+    <th>Contact</th>
+    <th>Country</th>
+  </tr>
+  <tr>
+    <td>
+        <?php 
+            $hi = 1;
+            $hoy = 3;
+                echo "dsada";
+                echo $hi + $hoy;
+
+                $yes = $hi + $hoy;
+                if (true) 
+                    echo "dasda"
+                
+        ?>
+    </td>
+    <td>Maria Anders</td>
+    <td>Germany</td>
+  </tr>
+  <tr>
+    <td>Centro comercial Moctezuma</td>
+    <td>Francisco Chang</td>
+    <td>Mexico</td>
+  </tr>   
+</body>
+</html>
